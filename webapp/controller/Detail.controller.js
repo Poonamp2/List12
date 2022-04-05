d72e1f699f9f8be8d1958e10876093d5e8c3fa0d
Poonam Patil <patilpoonam2399@gmail.com>
2022-04-05 08:30:08 +0000

    first commit

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller,Filter, FilterOperator) {
+        "use strict";
+
+        return Controller.extend("list2.controller.Detail", {
+            onInit: function () { 
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this); 
+                oRouter
+                    .getRoute("DetailView")
+                    .attachPatternMatched(this._onObjectMatched, this);
+                },
+                _onObjectMatched: function (oEvent) {
+                    var productId = oEvent.getParameter("arguments"). ProductId;
+                    this.doOperation(productId);
+                },
+                doOperation(productId){
+                    var that = this;
+                    var mProductModel = that.getOwnerComponent().getModel("mProducts");
+                    var aProducts = mProductModel.getProperty("/ProductCollection");
+                },
+            onPressNavBack: function(){
+              
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("RouteList");
+            }
+        });
+    });
---
+++ webapp/controller/List.controller.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller,Filter, FilterOperator, JSONModel) {
+        "use strict";
+
+        return Controller.extend("list2.controller.List", {
+            onInit: function () {
+            },
+            onSearchList: function(oEvent){
+                var sVal=oEvent.getSource().getValue(),
+                oBinding = this.byId("idlist").getBinding("items"),
+                oFilter;
+                if (sVal || sVal === "") {
+                oFilter = new Filter("SupplierName", FilterOperator.Contains, sVal);
+                oBinding.filter([oFilter]);
+                }
+                
+            },
+            onPressNext: function(){
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("DetailView",{ProductId:"0"});
+            },
+            onPressProduct: function(oEvent){
+                var that = this;
+                var sSelectedProduct = oEvent.getSource().getBindingContext("mProducts").getObject();
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("DetailView",{ProductId: sSelectedProduct.ProductId});
+            }
+        });
+    });
+
